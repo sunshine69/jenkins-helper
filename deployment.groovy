453c0e358080834a5809f571380bb5a308e60188
Steve Kieu <steve.kieu@xvt.com.au>
2019-10-02 16:10:25 +1000

    Update default ansible to 2.8.5

--- deployment.groovy
+++ deployment.groovy
@@ -104,7 +104,7 @@ EOF
 }
 
 def run_build_script(arg1=[:]) {
-    def default_arg = ['docker_net_opt': '--net=container:xvt', 'docker_volume_opt': '--volumes-from xvt_jenkins', 'docker_image': 'xvtsolutions/python3-aws-ansible:2.8.4', 'extra_build_scripts': [], 'run_as_user': [:] ]
+    def default_arg = ['docker_net_opt': '--net=container:xvt', 'docker_volume_opt': '--volumes-from xvt_jenkins', 'docker_image': 'xvtsolutions/python3-aws-ansible:2.8.5', 'extra_build_scripts': [], 'run_as_user': [:] ]
 
     def default_build_scripts = [
             'generate_add_user_script.sh',
